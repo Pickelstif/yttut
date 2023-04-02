b34484e287dc72b3800fd773020e2ac9b0b8ab81
Gregory Bell <ghbell@gmail.com>
2023-04-02 11:08:54 +0200

    v1.1

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     app = Flask(__name__)
     # future Greg - how to store this privately
     app.config['SECRET_KEY'] = 'mysecretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ngskdsxytrmkdr:546be030da6f96c57c291b79bf6070de80dbc695c7150af74dfb0dfbde7e61c7@ec2-44-213-151-75.compute-1.amazonaws.com:5432/de8b29nrnphif"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ngskdsxytrmkdr:546be030da6f96c57c291b79bf6070de80dbc695c7150af74dfb0dfbde7e61c7@ec2-44-213-151-75.compute-1.amazonaws.com:5432/de8b29nrnphif'
     db.init_app(app)
 
     from .views import views
