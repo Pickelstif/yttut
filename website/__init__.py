adb98677b3b4afadd797307b78ededdb4b3ecfb5
Gregory Bell <ghbell@gmail.com>
2023-04-02 12:57:12 +0200

    v1.1

--- website/__init__.py
+++ website/__init__.py
@@ -5,12 +5,21 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+# Google Cloud SQL (change this accordingly)
+PASSWORD ="(bmgA9{Ta>AahL~|"
+PUBLIC_IP_ADDRESS ="34.96.225.89"
+DBNAME ="database"
+PROJECT_ID ="yttut"
+INSTANCE_NAME ="big-keyword-382510:asia-east2:yttut"
+
 
 def create_app():
     app = Flask(__name__)
     # future Greg - how to store this privately
     app.config['SECRET_KEY'] = 'mysecretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
     db.init_app(app)
 
     from .views import views
@@ -21,7 +30,7 @@ def create_app():
 
     from .models import User, Note
 
-    #create_database(app)
+    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
