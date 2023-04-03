b1f18f29a7ac3b7fbc094944afb6d6698c11404f
Gregory Bell <ghbell@gmail.com>
2023-04-03 22:41:00 +0800

    v1.1

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -14,10 +15,15 @@ INSTANCE_NAME ="big-keyword-382510:asia-east2:yttut"
 
 
 def create_app():
+    basedir = os.path.join(os.path.pardir, "tmp")
+    print(basedir)
+    #os.path.dirname(os.path.abspath(__file__))
+
     app = Flask(__name__)
     # future Greg - how to store this privately
     app.config['SECRET_KEY'] = 'mysecretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
     #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 
     db.init_app(app)
