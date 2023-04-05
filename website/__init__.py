23e7add71679d81307d901ee68268bf26ea2c84a
Gregory Bell <ghbell@gmail.com>
2023-04-05 23:32:11 +0800

    v1.1

--- website/__init__.py
+++ website/__init__.py
@@ -1,57 +1,58 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-import os
-from google.cloud import storage
-
-
+#import os
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
-
-# Google Cloud SQL (change this accordingly)
-PASSWORD ="(bmgA9{Ta>AahL~|"
-PUBLIC_IP_ADDRESS ="34.96.225.89"
-DBNAME ="database"
-PROJECT_ID ="yttut"
-INSTANCE_NAME ="big-keyword-382510:asia-east2:yttut"
-
-GCLOUD_DB = "gs://big-keyword-382510.appspot.com/database.db"
-
 
 def create_app():
-    basedir = os.path.join(os.path.pardir, "tmp")
-
-    # Initialise a client
-    storage_client = storage.Client("yttut")
-    # Create a bucket object for our bucket
-    bucket = storage_client.bucket("big-keyword-382510.appspot.com")
-    # Create a blob object from the filepath
-    blob = bucket.blob("database.db")
-    # Download the file to a destination
-    blob.download_to_filename("./tmp/database.db")
-
-    #os.path.dirname(os.path.abspath(__file__))
+    # Google Cloud SQL (change this accordingly)
+    PASSWORD = "gflask"
+    PUBLIC_IP_ADDRESS = "34.122.44.173"
+    DBNAME = "yttut"
+    PROJECT_ID = "big-keyword-382510"
+    INSTANCE_NAME = "big-keyword-382510:us-central1:gflask"
 
     app = Flask(__name__)
-    # future Greg - how to store this privately
-    app.config['SECRET_KEY'] = 'mysecretkey'
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + GCLOUD_DB
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, DB_NAME)
-    #app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
-    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    # configuration
+    app.config["SECRET_KEY"] = "yoursecretkey"
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqldb://root:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
     db.init_app(app)
+
+    from .models import User, Note
+
+    with app.app_context():
+        db.create_all()
+
+    # basedir = os.path.join(os.path.pardir, "tmp")
+
+    # # Initialise a client
+    # storage_client = storage.Client("yttut")
+    # # Create a bucket object for our bucket
+    # bucket = storage_client.bucket("big-keyword-382510.appspot.com")
+    # # Create a blob object from the filepath
+    # blob = bucket.blob("database.db")
+    # # Download the file to a destination
+    # blob.download_to_filename("./tmp/database.db")
+    # ######DB URI    mysql+mysqldb://root@/database?unix_socket=/cloudsql/yttut:yttut
+    # #os.path.dirname(os.path.abspath(__file__))
+
+    # # future Greg - how to store this privately
+    # app.config['SECRET_KEY'] = 'mysecretkey'
+    # #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + GCLOUD_DB
+    # #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, DB_NAME)
+    # #app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqldb://root@/{DBNAME}?unix_socket=/cloudsql/{PROJECT_ID}:{INSTANCE_NAME}"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///../tmp/{DB_NAME}'
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
-
-    from .models import User, Note
-
-    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
@@ -62,8 +63,3 @@ def create_app():
         return User.query.get(int(id))
 
     return app
-
-
-def create_database(app):
-    with app.app_context():
-        db.create_all()
